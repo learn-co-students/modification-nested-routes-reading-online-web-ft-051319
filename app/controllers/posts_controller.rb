8423f1443bfeed8ae3dcf8549b434295f9b547b2
mkonig411blk <margot.konig@blackrock.com>
2019-07-30 16:03:30 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,14 @@ class PostsController < ApplicationController
     end
   end
 
-  def new
-    @post = Post.new
-  end
+ def new
+   if params[:author_id] && !Author.exists?(params[:author_id])
+     redirect_to authors_path, alert: "Author not found."
+   else
+     @post = Post.new(author_id: params[:author_id])
+   end
+ end
+ # Why aren't we doing a find_by and getting the author instance? Because we don't need a whole author instance for Post.new; we just need the author_id. And we don't need to check against the posts of the author because we're just creating a new one. 
 
   def create
     @post = Post.new(post_params)
@@ -33,12 +38,23 @@ class PostsController < ApplicationController
   end
 
   def edit
+  if params[:author_id]
+    author = Author.find_by(id: params[:author_id])
+    if author.nil?
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = author.posts.find_by(id: params[:id])
+      redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+    end
+    # If we do find the author, we next want to find the post by params[:id], but, instead of directly looking for Post.find(), we need to filter the query through our author.posts collection to make sure we find it in that author's posts. It may be a valid post id, but it might not belong to that author, which makes this an invalid request.
+  else
     @post = Post.find(params[:id])
   end
+end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
