1c0fe839e406c77758d9f7ed21b27d51f0a519da
Kane Koo <kanekoo@gmail.com>
2019-07-31 23:12:11 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,13 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      # else instantiate a new post (not save it yet though...sort of giving a pointer/placeholder
+      @post = Post.new(author_id: params[:author_id])
+    end
+    # @post = Post.new
   end
 
   def create
@@ -33,12 +39,23 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id]) # don't need to check :id
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      # the case when you are going to "/posts" directly
+      @post = Post.find(params[:id])
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
