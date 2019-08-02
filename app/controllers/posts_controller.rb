7d51f0b9e9fd7b8d4fd4be25ff300e57248b1f86
angiemalaika <malaika.mumba@gmail.com>
2019-08-02 15:59:03 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,17 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
-  end
+   if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else   
+      @post = Post.new(author_id: params[:author_id])
+    end 
+  end	  
+  # def new
+  #   @post = Post.new(author_id: params[:author_id] )
+
+  #   # if we capture an author_id through a nested route, we keep track of it and assign the post to that author
+  # end
 
   def create
     @post = Post.new(post_params)
@@ -28,17 +37,27 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+    @post.update(post_params)
     redirect_to post_path(@post)
   end
 
   def edit
-    @post = Post.find(params[:id])
-  end
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else 
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end 
+    else 
+   	    @post = Post.find(params[:id])
+    end 
+  end	 
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
