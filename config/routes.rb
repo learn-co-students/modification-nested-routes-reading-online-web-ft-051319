c9a5383de594ae75af4216b0c2dc1600d73d0232
Natalie <natsetc@gmail.com>
2019-08-05 19:16:53 -0700

    add routes for new, create, update

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :authors, only: [:show, :index] do 
     resources :posts, only: [:show, :index, :new, :edit]
   end
-  resources :posts, only: [:new]
+  resources :posts, only: [:new, :create, :update]
 end
